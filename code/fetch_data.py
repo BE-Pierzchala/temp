375162ed65343d3c03510ec94018b0c6d039e604
bartek Ubuntu <bartek@techko.eu>
2026-03-29 19:34:22 +0200

    Handling disconnect

--- code/fetch_data.py
+++ code/fetch_data.py
@@ -6,5 +6,7 @@ def get_data(ip: str) -> Tuple[float, int]:
         response = requests.get(f"http://{ip}/data").json()
     except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
         return None, None
+    if response["temperature"] == 0:
+        return None, None
     return response['temperature'], response['humidity']
 
